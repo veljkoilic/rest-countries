3a0c24d4146e21b3e5e6d80cb7c55c00666694e6
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-10 16:57:34 +0200

    dynamic data

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,74 +1,111 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { tablet } from "../responsive";
 
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import { setCountry } from "../redux/countrySlice";
+import { useEffect } from "react";
+
 export const Country = () => {
   const navigate = useNavigate();
-  return (
-    <Container>
-      <Back
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        <FontAwesomeIcon icon={faArrowLeft} />
-        Back
-      </Back>
-      <CountryWrap>
-        <Image src="https://upload.wikimedia.org/wikipedia/en/b/ba/Flag_of_Germany.svg" />
-        <CountryInfo>
-          <Title>Germany</Title>
-          <Top>
-            <Left>
+
+  const dispatch = useDispatch();
+  const country = useSelector((state) => state.countries.currentCountry)[0];
+  const query = useParams().id;
+  useEffect(() => {
+    try {
+      axios.get(`https://restcountries.com/v3.1/name/${query}?fullText=true`).then(function (response) {
+        dispatch(setCountry(response.data));
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  let currencies = [];
+  let languages = [];
+  let nativeNames = [];
+  if (country) {
+    for (const key in country.currencies) {
+      currencies.push(`${country.currencies[key].name}`);
+    }
+    for (const key in country.languages) {
+      languages.push(`${country.languages[key]}`);
+    }
+    for (const key in country.name.nativeName) {
+      nativeNames.push(`${country.name.nativeName[key].official}`);
+    }
+  }
+  console.log(country);
+  if (country) {
+    return (
+      <Container>
+        <Back
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+          Back
+        </Back>
+        <CountryWrap>
+          <Image src={country.flags.png} />
+          <CountryInfo>
+            <Title>{country.name.official}</Title>
+            <Top>
+              <Left>
+                <span>
+                  <span className="bold">Native Names:</span> {nativeNames.join(", ")}
+                </span>
+                <span>
+                  <span className="bold">Population:</span>{" "}
+                  {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </span>
+                <span>
+                  <span className="bold">Region:</span> {country.region}
+                </span>
+                <span>
+                  <span className="bold">Sub Region:</span> {country.subregion}
+                </span>
+                <span>
+                  <span className="bold">Capital:</span> {country.capital[0]}
+                </span>
+              </Left>
+              <Right>
+                <span>
+                  <span className="bold">Top Level Domain:</span> {country.tld}
+                </span>
+                <span>
+                  <span className="bold">Currencies:</span> {currencies.join(", ")}
+                </span>
+                <span>
+                  <span className="bold">Languages:</span> {languages.join(", ")}
+                </span>
+              </Right>
+            </Top>
+            <Bottom>
               <span>
-                <span className="bold">Native Name:</span> Germy
+                <span className="bold">Border Countries: {!country.borders && "None"}</span>
               </span>
-              <span>
-                <span className="bold">Population:</span> 1.0000.000{" "}
-              </span>
-              <span>
-                <span className="bold">Region:</span> Europe
-              </span>
-              <span>
-                <span className="bold">Sub Regioon:</span> Western Europe
-              </span>
-              <span>
-                <span className="bold">Capital:</span> Berlin
-              </span>
-            </Left>
-            <Right>
-              <span>
-                <span className="bold">Top Level Domain:</span> .de
-              </span>
-              <span>
-                <span className="bold">Currencies:</span> Euro{" "}
-              </span>
-              <span>
-                <span className="bold">Languages:</span> Dutch, French, German
-              </span>
-            </Right>
-          </Top>
-          <Bottom>
-            <span>
-              <span className="bold">Border Countries:</span>
-            </span>
-            <Link className="link" to={"/country/2"}>
-              <BorderCountry>France</BorderCountry>
-            </Link>
-            <Link className="link" to={"/country/2"}>
-              <BorderCountry>Austria</BorderCountry>
-            </Link>
-            <Link className="link" to={"/country/2"}>
-              <BorderCountry>Poland</BorderCountry>
-            </Link>
-          </Bottom>
-        </CountryInfo>
-      </CountryWrap>
-    </Container>
-  );
+              {country.borders &&
+                country.borders.map((c) => {
+                  return (
+                    <Link className="link" to={"/country/"}>
+                      <BorderCountry>{c}</BorderCountry>
+                    </Link>
+                  );
+                })}
+            </Bottom>
+          </CountryInfo>
+        </CountryWrap>
+      </Container>
+    );
+  } else {
+    return <h1 style={{ marginTop: "20px", textAlign: "center" }}>Getting data...</h1>;
+  }
 };
 
 const Container = styled.div`
@@ -93,6 +130,7 @@ const Back = styled.button`
 `;
 
 const CountryWrap = styled.div`
+  margin-top: 50px;
   display: flex;
   justify-content: space-between;
   ${tablet({
@@ -101,18 +139,20 @@ const CountryWrap = styled.div`
   })}
 `;
 const Image = styled.img`
-  width: 40%;
+  width: 35%;
   padding: 50px 100px 0 0;
-  flex: 1;
+  /* flex: 1; */
   ${tablet({
     paddingRight: 0,
-    width: "80%"
+    width: "80%",
   })}
 `;
 const CountryInfo = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1;
+  justify-content: space-between;
+  width: 50%;
+  object-fit: contain;
   ${tablet({
     width: "80%",
   })}
@@ -125,13 +165,14 @@ const Title = styled.h1`
 `;
 const Top = styled.div`
   display: flex;
-  gap: 40px;
+  gap: 50px;
   ${tablet({
     flexDirection: "column",
     width: "80%",
   })}
   span {
     padding: 5px 0;
+    font-size: 16px;
   }
 `;
 const Left = styled.div`
@@ -148,10 +189,11 @@ const Bottom = styled.div`
   margin-top: 20px;
   display: flex;
   align-items: center;
+  flex-wrap:wrap;
   gap: 20px;
   ${tablet({
     flexDirection: "column",
-    alignItems: "flex-start"
+    alignItems: "flex-start",
   })}
 `;
 const BorderCountry = styled.div`
