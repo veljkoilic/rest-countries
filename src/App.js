1317089322d9fc6d4800952cff23daf7e3ba0407
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-10 10:45:49 +0200

    router, routes, routes, components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// - See all countries from the API on the homepage
+// - Search for a country using an `input` field
+// - Filter countries by region
+// - Click on a country to see more detailed information on a separate page
+// - Click through to the border countries on the detail page
+// - Toggle the color scheme between light and dark mode *(optional)*
+
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {} from "react-router";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Country } from "./pages/Country";
+import styled from "styled-components";
+import { useSelector } from "react-redux/es/exports";
 
 function App() {
+  const colorMode = useSelector(state=>state.theme.theme)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" color-mode={colorMode ? "light" : "dark"}>
+      <Container>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/country/:id" element={<Country />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
     </div>
   );
 }
+const Container = styled.div`
+background-color: var(--bg-color);
+color: var(--text-color)
+`;
 
 export default App;
