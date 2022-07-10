1db9402eeec02380da2a14e0f28f9c1204eafeef
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-10 16:58:18 +0200

    filtering added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { CountryCard } from "../components/CountryCard";
 import { SearchAndFilter } from "../components/SearchAndFilter";
-import {laptop ,mobile, tablet} from '../responsive'
+import { laptop, mobile, tablet } from "../responsive";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { addCountries, filterCountries } from "../redux/countrySlice";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+  const countries = useSelector(state=>state.countries.countries)
+  const filteredCountries = useSelector(state=>state.countries.filteredCountries)
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then(function (response) {
+      dispatch(addCountries(response.data))
+    });
+  }, []);
   return (
     <Container>
       <SearchAndFilter />
       <CountryCardWrapper>
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
+        {filteredCountries.length > 0 && filteredCountries.map((c, index) => <CountryCard country={c} key={index}/>)}
+        {filteredCountries.length === 0 && countries.map((c, index) => <CountryCard country={c} key={index}/>)}
       </CountryCardWrapper>
     </Container>
   );
@@ -25,23 +30,20 @@ export const Home = () => {
 const Container = styled.div`
   padding: 5px 5%;
   padding-bottom: 100px;
-
 `;
 
 const CountryCardWrapper = styled.div`
-display: grid;
-grid-template-columns: repeat(4, 1fr);
-${laptop({
-  gridTemplateColumns: "repeat(3, 1fr);"
-})}
-${tablet({
-  gridTemplateColumns: "repeat(2, 1fr);"
-})}
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  ${laptop({
+    gridTemplateColumns: "repeat(3, 1fr);",
+  })}
+  ${tablet({
+    gridTemplateColumns: "repeat(2, 1fr);",
+  })}
 ${mobile({
-  gridTemplateColumns: "1fr"
-})}
+    gridTemplateColumns: "1fr",
+  })}
 gap: 30px;
-margin: 100px auto 0 auto;
-
-
+  margin: 100px auto 0 auto;
 `;
